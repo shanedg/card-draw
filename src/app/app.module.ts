9b701817b82810067a8b9ae6d93a6a68b18e5cd9
Shane Garrity <shanedgarrity@gmail.com>
2018-09-23 19:59:23 -0400

    ng generated tests passing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
 
 import { MatSelectModule } from '@angular/material/select';
 import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
 import { FiltersComponent } from './filters/filters.component';
 import { HandComponent } from './hand/hand.component';
 
@@ -23,7 +24,10 @@ import 'hammerjs';
     BrowserAnimationsModule,
     FormsModule,
     MatSelectModule,
-    MatSliderModule
+    MatSliderModule,
+  ],
+  schemas: [
+    // NO_ERRORS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
