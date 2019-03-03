785c831d7df3972338b9e6bea7a1f626ff50f87d
shane garrity <shanedgarrity@gmail.com>
2019-03-02 20:00:39 -0500

    feat: add simple e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { protractor } from 'protractor/built/ptor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -11,4 +12,18 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to card-draw!');
   });
+
+  it('should draw a hand of 7 cards', () => {
+    page.navigateTo();
+    page.getButton().click();
+    expect(page.getHand().count()).toEqual(7);
+  });
+
+  it('should draw 0 cards if 0 decks selected', () => {
+    page.navigateTo();
+    page.getDecksToDraw().sendKeys(protractor.Key.BACK_SPACE, 0);
+    page.getButton().click();
+    expect(page.getHand().count()).toEqual(0);
+  });
+
 });
